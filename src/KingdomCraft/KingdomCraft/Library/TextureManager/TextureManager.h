cf1af166d0b6f18adfc50ca949f12ce89139c3a8
Moto0116 <moto01160902@gmail.com>
2016-10-20 23:32:21 +0900

    #37 TextureManagerクラスにコメントを付与

--- src/KingdomCraft/KingdomCraft/Library/TextureManager/TextureManager.h
+++ src/KingdomCraft/KingdomCraft/Library/TextureManager/TextureManager.h
@@ -9,13 +9,28 @@ public:
 	TextureManager(ID3D11Device* _pDevice);
 	~TextureManager();
 
+	/**
+	 * テクスチャを読み込む
+	 * @param[in] _key 読み込んだテクスチャの格納先へのキー
+	 * @param[in] _filePath 読み込むテクスチャ
+	 * @return テクスチャの読み込みに成功したらtrue
+	 */
 	bool LoadTexture(int _key, char* _filePath);
 
+	/**
+	 * 格納しているテクスチャを取得する
+	 * @param[in] _key 取得したいテクスチャのキー
+	 * @return テクスチャのリソースビュー
+	 */
 	inline ID3D11ShaderResourceView* GetTexture(int _key)
 	{ 
 		return m_pTextureResourceView[_key];
 	}
 	
+	/**
+	 * 格納しているテクスチャを解放する
+	 * @param[in] _key 解放したいテクスチャのキー
+	 */
 	inline void ReleaseTexture(int _key)
 	{ 
 		m_pTextureResourceView[_key]->Release();
