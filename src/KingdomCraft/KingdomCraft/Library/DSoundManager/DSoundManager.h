82a0e00a0204d65b71a870b0e410bd718277a83f
Keitaro0408 <kei.kotani77@gmail.com>
2016-11-08 11:00:25 +0900

    DSoundManagerクラスの機能修正とコメント修正
    - [issue #89] DSoundManagerクラスのDoxygenコメントを修正
    - [issue #116] DSoundManagerのデータの格納方法を変更

--- src/KingdomCraft/KingdomCraft/Library/DSoundManager/DSoundManager.h
+++ src/KingdomCraft/KingdomCraft/Library/DSoundManager/DSoundManager.h
@@ -1,8 +1,13 @@
-﻿#ifndef DXSOUNDMANAGER_H
+﻿/**
+ * @file   DSoundManager.h
+ * @brief  DSoundManagerクラスのヘッダファイル
+ * @author kotani
+ */
+#ifndef DXSOUNDMANAGER_H
 #define DXSOUNDMANAGER_H
 #include <windows.h>
 #include <dsound.h>
-#include <map>
+#include <vector>
 
 enum SOUND_OPERATION
 {
@@ -13,46 +18,51 @@ enum SOUND_OPERATION
 	SOUND_STOP_RESET
 };
 
+/**
+ * @brief サウンドの再生をするクラス
+ *
+ * ここでサウンドのリソースを管理し実体は一つで良いのでシングルトンクラスにしている
+ */
 class DSoundManager
 {
 public:
 	
 	/**
-	* 初期化処理
-	* @return 成功したらtrue
-	*/
+	 * 初期化処理
+	 * @return 成功したらtrue
+	 */
 	bool Init();
 
 	/**
-	* 解放処理
-	*/
-	void Release(){ m_pDSound8->Release(); };
+	 * 解放処理
+	 */
+	inline void Release(){ m_pDSound8->Release(); }
 
 	/**
-	* 音声の読み込み
-	* @param[in] _key 格納先のキー
-	* @param[in] _filename 読み込むファイルの名前
-	* @return 成功したらtrue
-	*/
-	bool SoundLoad(int _key, char* _filename);
+	 * 音声の読み込み
+	 * @param[in]  _filename 読み込むファイルの名前
+	 * @param[out] _pKey データの格納先のキー
+	 * @return	   成功したらtrue
+	 */
+	bool LoadSound(char* _filename, int* _pKey);
 	
 	/**
-	* 音声の開放
-	* @param[in] _key 開放先のキー
-	*/
-	void ReleaseSound(int _key);
+	 * 音声の開放
+	 * @param[in] _key 開放先のキー
+	 */
+	inline void ReleaseSound(int _key){ m_pSound[_key]->Release(); }
 
 	/**
-	* サウンドの操作関数
-	* @param[in] _key 操作するサウンドの格納先のキー
-	* @param[in] operation どのような操作をするか
-	*/
+	 * サウンドの操作関数
+	 * @param[in] _key 操作するサウンドの格納先のキー
+	 * @param[in] _operation どのような操作をするか
+	 */
 	void SoundOperation(int _key, SOUND_OPERATION _operation);
 
 	/**
-	* インスタンス生成関数
-	* @param[in] _hWnd ウィンドウハンドル。コンストラクタ内で使う
-	*/
+	 * インスタンス生成関数
+	 * @param[in] _hWnd ウィンドウハンドル。コンストラクタ内で使う
+	 */
 	static void Create(HWND _hWnd)
 	{
 		if (m_pSoundManager == NULL)
@@ -60,10 +70,15 @@ public:
 			m_pSoundManager = new DSoundManager(_hWnd);
 		}
 	}
+
+	/**
+	 * サウンドのバッファーを開放する
+	 */
+	inline void ClearBuffer(){ m_pSound.clear(); }
 	
 	/**
-	* @return インスタンスの破棄
-	*/
+	 * インスタンスの破棄
+	 */
 	static void Destroy()
 	{
 		if (m_pSoundManager != NULL)
@@ -74,8 +89,9 @@ public:
 	}
 
 	/**
-	* @return インスタンスが返る
-	*/
+	 * インスタンスを取得する
+	 * @return インスタンスが返る
+	 */
 	static DSoundManager* GetInstance()
 	{
 		return m_pSoundManager;
@@ -84,10 +100,10 @@ public:
 private:
 	DSoundManager(HWND _hWnd);
 	~DSoundManager();
-	HWND								m_hWnd;
-	IDirectSound8*						m_pDSound8;
-	std::map<int, LPDIRECTSOUNDBUFFER8> m_SoundMap;
-	static DSoundManager*				m_pSoundManager;
-	bool								OpenWave(TCHAR* _filename, WAVEFORMATEX &_wFmt, char** _pWaveData, DWORD &_waveSize);
+	HWND							  m_hWnd;
+	IDirectSound8*					  m_pDSound8;
+	std::vector<LPDIRECTSOUNDBUFFER8> m_pSound;
+	static DSoundManager*			  m_pSoundManager;
+	bool							  OpenWave(TCHAR* _filename, WAVEFORMATEX &_wFmt, char** _pWaveData, DWORD &_waveSize);
 };
 #endif
