1fe33dc334d6bd7d262bf92229e72eacfa77eed5
Moto0116 <moto01160902@gmail.com>
2016-11-11 17:32:58 +0900

    [issue #131] SceneManager内の内部処理を追加

--- src/KingdomCraft/KingdomCraft/Main/SceneManager/SceneManager.cpp
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/SceneManager.cpp
@@ -1,6 +1,12 @@
 ﻿#include "SceneManager.h"
-#include "Scene//Scene.h"
-#include "SceneFactory//SceneFactory.h"
+#include "Scene\Scene.h"
+#include "SceneFactory\SceneFactory.h"
+#include "DX11Manager\DX11Manager.h"
+#include "InputDeviceManager\InputDeviceManager.h"
+#include "TextureManager\TextureManager.h"
+#include "ShaderManager\ShaderManager.h"
+#include "DSoundManager\DSoundManager.h"
+#include "FbxFileManager\FbxFileManager.h"
 
 SceneManager::SceneManager(HWND _hwnd) :
 m_pScene(NULL),
@@ -10,10 +16,41 @@ m_nextSceneID(SceneID::SCENE_TITLE),
 m_end(false),
 m_hWnd(_hwnd)
 {
+	DX11Manager::Create();
+	DX11Manager::GetInstance()->Init(m_hWnd);
+
+	InputDeviceManager::Create();
+	InputDeviceManager::GetInstance()->Init(m_hWnd);
+	InputDeviceManager::GetInstance()->CreateKeyDevice();
+	InputDeviceManager::GetInstance()->CreateMouseDevice();
+
+	TextureManager::Create(DX11Manager::GetInstance()->GetDevice());
+
+	ShaderManager::Create(DX11Manager::GetInstance()->GetDevice());
+
+	DSoundManager::Create(m_hWnd);
+
+	FbxFileManager::Create(DX11Manager::GetInstance()->GetDevice(), DX11Manager::GetInstance()->GetDeviceContext());
+	FbxFileManager::GetInstance()->Init();
 }
 
 SceneManager::~SceneManager()
 {
+	FbxFileManager::GetInstance()->Release();
+	FbxFileManager::Delete();
+
+	DSoundManager::Destroy();
+	
+	ShaderManager::Delete();
+
+	TextureManager::Delete();
+
+	InputDeviceManager::GetInstance()->Release();
+	InputDeviceManager::Delete();
+
+	DX11Manager::GetInstance()->Release();
+	DX11Manager::Delete();
+
 	delete m_pScene;
 	delete m_pSceneFactory;
 }
