58204adc2561e3c13f825d33b405419958b55094
Moto0116 <moto01160902@gmail.com>
2016-11-18 15:24:18 +0900

    [issue #154] DebugFontクラスのリファクタリングと修正
    - 変数名の修正
    - 引数にconstを付与
    - 仮引数名の修正
    - 自動キャストを明示的にキャストするように変更
    - .hのけつに改行追加
    - 関数の返り値に対するコメントを追加
    - 関数引数のconstをいくつか外した
    - Initで受けてた引数をコンストラクタで受けるようにした。

--- src/KingdomCraft/KingdomCraft/Library/DebugFont/DebugFont.h
+++ src/KingdomCraft/KingdomCraft/Library/DebugFont/DebugFont.h
@@ -8,25 +8,30 @@
 #include <d3d11.h>
 #include <d3dx10.h>
 
-#define FONT_TU 1.0f / 95
-
 /**
- * @brief  デバッグ情報を表示するクラス
+ * デバッグ情報を表示するクラス
  */
 class DebugFont
 {
 public:
-	DebugFont();
+	/**
+	 * DebugFontクラスのコンストラクタ
+	 * @param[in] _pDevice DirectX11のデバイス
+	 * @param[in] _pDeviceContext DirectX11のデバイスコンテキスト
+	 */
+	DebugFont(ID3D11Device* _pDevice, ID3D11DeviceContext* _pDeviceContext, HWND _hWnd);
+
+	/**
+	 * DebugFontクラスのデストラクタ
+	 */
 	~DebugFont();
 
 	/**
 	 * DebugFontクラスの初期化関数
-	 * @param[in] _pDevice DirectX11のデバイス
-	 * @param[in] _pDeviceContext DirectX11のデバイスコンテキスト
 	 * @param[in] _hWnd DebugFontクラスが対応するWindowのハンドル
 	 * @return 初期化に成功したらtrueが返る
 	 */
-	bool Init(ID3D11Device* _pDevice, ID3D11DeviceContext* _pDeviceContext, HWND _hWnd);
+	bool Init();
 
 	/**
 	 * DebugFontクラスの解放関数
@@ -35,9 +40,10 @@ public:
 
 	/**
 	 * フォントの初期化
-	 * @param[in] _FontHeight フォントの高さ
-	 * @param[in] _FontWidth フォントの幅
-	 * @param[in] _FontColor フォントの色
+	 * @param[in] _fontHeight フォントの高さ
+	 * @param[in] _fontWidth フォントの幅
+	 * @param[in] _fontColor フォントの色
+	 * @return 初期化に成功したらtrueが返る
 	 */
 	bool InitFont(float _fontHeight, float _fontWidth, D3DXCOLOR _fontColor);
 
@@ -46,45 +52,122 @@ public:
 	 * @param[in] _DrawPos フォントを表示する位置
 	 * @param[in] _pStr 表示する文字列
 	 */
-	void DrawFont(D3DXVECTOR2* _pDrawPos, char* _pStr);
+	void DrawFont(const D3DXVECTOR2* _pDrawPos, LPCTSTR _pStr);
 
 private:
+	/**
+	 * フォント表示のための頂点構造体
+	 */
 	struct FONT_VERTEX
 	{
-		D3DXVECTOR3 Pos;
-		D3DXCOLOR	Color;
-		float		tu;
-		float		tv;
+		D3DXVECTOR3 Pos;	//!< 頂点座標
+		D3DXCOLOR	Color;	//!< 頂点カラー
+		D3DXVECTOR2	Texel;	//!< テクスチャ座標
 	};
 
+	/**
+	 * シェーダーに渡す定数バッファ構造体
+	 */
 	struct CONSTANT_BUFFER
 	{
-		D3DXMATRIX MatWVP;
-		D3DXVECTOR4 TexelOffset;
-		D3DXVECTOR4 PosOffset;
+		D3DXMATRIX MatWVP;		//!< ワールド変換行列
+		D3DXVECTOR4 TexelOffset;//!< テクスチャ座標のオフセット
+		D3DXVECTOR4 PosOffset;	//!< 頂点座標のオフセット
 	};
 
+	/**
+	 * リソースビューの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitResourceView();
+
+	/**
+	 * 定数バッファの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitConstantBuffer();
+
+	/**
+	 * シェーダーの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitShader();
+
+	/**
+	 * 頂点シェーダーの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitVertexShader();
+
+	/**
+	 * ピクセルシェーダーの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitPixelShader();
+
+	/**
+	 * サンプラステートの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitSamplerState();
+
+	/**
+	 * ブレンドステートの初期化関数
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitBlendState();
+
+	/**
+	 * 深度ステンシルバッファの作成
+	 * @return 初期化に成功したらtrue
+	 */
 	bool InitDepthStencilState();
 
+	/**
+	 * リソースビューの解放関数
+	 */
 	void ReleaseResourceView();
+
+	/**
+	 * 定数バッファの解放関数
+	 */
 	void ReleaseConstantBuffer();
+
+	/**
+	 * シェーダーの解放関数
+	 */
 	void ReleaseShader();
+
+	/**
+	 * 頂点シェーダーの解放関数
+	 */
 	void ReleaseVertexShader();
+
+	/**
+	 * ピクセルシェーダーの解放関数
+	 */
 	void ReleasePixelShader();
+
+	/**
+	 * サンプラステートの解放関数
+	 */
 	void ReleaseSamplerState();
+
+	/**
+	 * ブレンドステートの解放関数
+	 */
 	void ReleaseBlendState();
+
+	/**
+	 * 深度ステンシルバッファの解放関数
+	 */
 	void ReleaseDepthStencilState();
 
 
-	ID3D11Device*				m_pDevice;
-	ID3D11DeviceContext*		m_pDeviceContext;
+	static const float m_DebugFontTu;
+	static const int m_SpaceAsciiCode;
+	ID3D11Device* const			m_pDevice;
+	ID3D11DeviceContext* const	m_pDeviceContext;
 	ID3D11ShaderResourceView*	m_pResourceView;
 	ID3D11Buffer*				m_pConstantBuffer;
 	ID3D11Buffer*				m_pVertexBuffer;
@@ -94,7 +177,7 @@ private:
 	ID3D11SamplerState*			m_pSamplerState;
 	ID3D11BlendState*			m_pBlendState;
 	ID3D11DepthStencilState*	m_pDepthStencilState;
-	HWND						m_hWnd;
+	HWND const					m_hWnd;
 	float						m_FontHeight;
 	float						m_FontWidth;
 	float						m_WindowWidth;
