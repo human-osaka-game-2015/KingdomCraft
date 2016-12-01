7db929d34c4d3bbb8b372fccba83c1116bc495e0
Keitaro0408 <kei.kotani77@gmail.com>
2016-12-01 23:02:56 +0900

    [issue #59] GameScene配下実装
    - ObjectManagerクラス追加
    - GameObjectManagerクラス追加
    - UIManagerクラス追加

---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/ObjectManager.h
@@ -0,0 +1,48 @@
+﻿/**
+ * @file   ObjectManager.h
+ * @brief  ObjectManagerクラスのヘッダファイル
+ * @author kotani
+ */
+#ifndef OBJECTMANAGER_H
+#define OBJECTMANAGER_H
+
+class UIManager;
+class GameObjectManager;
+
+/**
+ * ゲームのオブジェクトを管理するクラス
+ */
+class ObjectManager
+{
+public:
+	/**
+	 * ObjectManagerのコンストラクタ
+	 */
+	ObjectManager();
+
+	/**
+	 * ObjectManagerのデストラクタ
+	 */
+	~ObjectManager();
+
+	/**
+	 * ObjectManagerの制御関数
+	 */
+	void Control();
+
+	/**
+	 * ObjectManagerの描画関数
+	 */
+	void Draw();
+
+private:
+	ObjectManager(const ObjectManager&);
+	void operator=(const ObjectManager&);
+
+	GameObjectManager* m_pGameObjectManager;
+	UIManager*		   m_pUIManager;
+
+};
+
+
+#endif
---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/UIManager.h
@@ -0,0 +1,42 @@
+﻿/**
+ * @file   UIManager.h
+ * @brief  UIManagerクラスのヘッダファイル
+ * @author kotani
+ */
+#ifndef UIMANAGER_H
+#define UIMANAGER_H
+
+/**
+ * UIを管理するクラス
+ */
+class UIManager
+{
+public:
+	/**
+	 * UIManagerのコンストラクタ
+	 */
+	UIManager();
+
+	/**
+	 * UIManagerのデストラクタ
+	 */
+	~UIManager();
+
+	/**
+	 * UIManagerの制御関数
+	 */
+	void Control();
+
+	/**
+	 * UIManagerの描画関数
+	 */
+	void Draw();
+
+private:
+	UIManager(const UIManager&);
+	void operator=(const UIManager&);
+
+};
+
+
+#endif
