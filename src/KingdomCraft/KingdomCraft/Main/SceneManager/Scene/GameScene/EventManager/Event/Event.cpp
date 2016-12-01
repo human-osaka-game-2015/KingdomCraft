677f3c7a2d2b484285edc22faf924d6dbb680d4f
Moto0116 <moto01160902@gmail.com>
2016-12-01 13:13:13 +0900

    [issue #176] EventManager配下のクラス群作成
    - EventManagerクラスの追加
    - EventListenerクラスの追加
    - Eventクラスの追加

---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/EventManager/Event/Event.cpp
@@ -0,0 +1,19 @@
+﻿/**
+ * @file Event.cpp
+ * @brief Eventクラス実装
+ * @author morimoto
+ */
+#include "Event.h"
+
+const int Event::m_ErrorID = -1;
+int Event::m_EventNum = 0;
+
+
+Event::Event()
+{
+	m_EventNum++;
+}
+
+Event::~Event()
+{
+}
