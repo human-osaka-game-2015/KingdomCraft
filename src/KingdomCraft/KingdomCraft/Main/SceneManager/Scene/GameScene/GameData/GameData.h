a84cf3f31ebd86b0d82a9fdd45d2e77bcb07f085
Moto0116 <moto01160902@gmail.com>
2016-12-01 13:17:28 +0900

    [issue #181] GameDataクラスの追加

---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/GameData/GameData.h
@@ -0,0 +1,61 @@
+﻿/**
+ * @file GameData.h
+ * @brief GameDataクラスのヘッダファイル
+ * @author morimoto
+ */
+#ifndef GAMEDATA_H
+#define GAMEDATA_H
+#include <Windows.h>
+
+/**
+ * GameScene内のデータを管理するクラス
+ */
+class GameData
+{
+public:
+	/**
+	 * インスタンスを生成する関数
+	 */
+	static void Create()
+	{
+		if (m_pGameData == NULL)
+		{
+			m_pGameData = new GameData();
+		}
+	}
+
+	/**
+	 * インスタンスを取得する関数
+	 * @return GameDataクラスのインスタンス
+	 */
+	static GameData* GetInstance()
+	{
+		return m_pGameData;
+	}
+
+	/**
+	 * インスタンスを破棄する関数
+	 */
+	static void Delete()
+	{
+		delete m_pGameData;
+		m_pGameData = NULL;
+	}
+
+private:
+	/**
+	 * GameDataクラスのコンストラクタ
+	 */
+	GameData();
+
+	/**
+	 * GameDataクラスのデストラクタ
+	 */
+	~GameData();
+
+	static GameData* m_pGameData;
+
+};
+
+
+#endif
