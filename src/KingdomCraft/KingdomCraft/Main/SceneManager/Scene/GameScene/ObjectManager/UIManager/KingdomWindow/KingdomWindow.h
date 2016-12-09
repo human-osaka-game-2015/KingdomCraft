f0482c619c13bd59e78f1a905c10f9bf6d4ab674
Moto0116 <moto01160902@gmail.com>
2016-12-09 16:47:37 +0900

    [issue #192] UIManagerクラス配下の追加
    - UIWindowクラスの追加
    - UIButtonクラスの追加
    - GameTimeWindowクラスの追加
    - KingdomWindowクラスの追加
    - OperationWindowクラスの追加

---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomWindow/KingdomWindow.h
@@ -0,0 +1,46 @@
+﻿/**
+ * @file   KingdomWindow.h
+ * @brief  KingdomWindowクラスのヘッダファイル
+ * @author morimoto
+ */
+#ifndef KINGDOMWINDOW_H
+#define KINGDOMWINDOW_H
+#include "..\UIWindow\UIWindow.h"
+
+/**
+ * 王国のステータスUI管理クラス
+ */
+class KingdomWindow : public UIWindow
+{
+public:
+	/**
+	 * KingdomWindowクラスのコンストラクタ
+	 */
+	KingdomWindow();
+
+	/**
+	 * KingdomWindowクラスのデストラクタ
+	 */
+	virtual ~KingdomWindow();
+
+	/**
+	 * KingdomWindowクラスの制御関数
+	 */
+	virtual void Control();
+
+	/**
+	 * KingdomWindowクラスの描画関数
+	 */
+	virtual void Draw();
+
+private:
+	KingdomWindow(const KingdomWindow&);
+	void operator=(const KingdomWindow&);
+
+	static const D3DXVECTOR2	m_KingdomWindowSize;
+	static const D3DXVECTOR2	m_KingdomWindowPos;
+
+};
+
+
+#endif
