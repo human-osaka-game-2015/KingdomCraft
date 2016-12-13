eed5ad67b9a2488f39501d284fdf665d1cd8c9d6
Moto0116 <moto01160902@gmail.com>
2016-12-13 12:10:46 +0900

    TextUIクラスの実装
    - [issue #220] TextUIクラスの実装
    	- TextUIクラスの追加
    - [issue #225] TextUIクラスの修正
    	- フォントサイズの指定が縦横逆だったのを修正

---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/TextUI/TextUI.cpp
@@ -0,0 +1,41 @@
+﻿/**
+ * @file   TextUI.cpp
+ * @brief  TextUIクラスの実装
+ * @author morimoto
+ */
+#include "TextUI.h"
+#include "DX11Manager\DX11Manager.h"
+
+
+TextUI::TextUI(const D3DXVECTOR2* _pTextPos, const D3DXVECTOR2* _pFontSize, bool _isVisible) :
+m_TextPos(*_pTextPos),
+m_FontSize(*_pFontSize),
+m_IsVisible(_isVisible)
+{
+	m_pDebugFont = new DebugFont(
+		DX11Manager::GetInstance()->GetDevice(),
+		DX11Manager::GetInstance()->GetDeviceContext());
+
+	m_pDebugFont->Init(DX11Manager::GetInstance()->GetWindowHandle());
+
+	m_pDebugFont->InitFont(m_FontSize.y, m_FontSize.x, 0xffffffff);
+}
+
+TextUI::~TextUI()
+{
+	m_pDebugFont->Release();
+	delete m_pDebugFont;
+}
+
+void TextUI::Control()
+{
+}
+
+void TextUI::Draw()
+{
+}
+
+void TextUI::TextDraw(LPCTSTR _pStr)
+{
+	m_pDebugFont->DrawFont(&m_TextPos, _pStr);
+}
