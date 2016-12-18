90fadfc4b2c066e0cf7e4ac2b63ea3e63c663d3d
Moto0116 <moto01160902@gmail.com>
2016-12-19 01:58:08 +0900

    KingdomWindowクラス配下の追加
    - [issue #213] KindomWindow配下のクラス追加
    	- KingdomDataUIクラス追加
    	- KingdomDemandUIクラス追加
    	- KingdomParameteUIrクラス追加
    	- KingdomButtonUIクラス追加

---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomWindow/KingdomDataUI/KingdomDataUI.cpp
@@ -0,0 +1,28 @@
+﻿/**
+ * @file   KingdomDataUI.cpp
+ * @brief  KingdomDataUIクラスの実装
+ * @author morimoto
+ */
+#include "KingdomDataUI.h"
+
+const D3DXVECTOR2 KingdomDataUI::m_TextPos = D3DXVECTOR2(700, 160);
+const D3DXVECTOR2 KingdomDataUI::m_FontSize = D3DXVECTOR2(32, 16);
+
+
+KingdomDataUI::KingdomDataUI() :
+TextUI(&m_TextPos, &m_FontSize)
+{
+}
+
+KingdomDataUI::~KingdomDataUI()
+{
+}
+
+void KingdomDataUI::Control()
+{
+}
+
+void KingdomDataUI::Draw()
+{
+	TextDraw("KingdomDataUI");
+}
---
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomWindow/KingdomParameterUI/KingdomParameterUI.cpp
@@ -0,0 +1,48 @@
+﻿/**
+ * @file   KingdomParameterUI.cpp
+ * @brief  KingdomParameterUIクラスの実装
+ * @author morimoto
+ */
+#include "KingdomParameterUI.h"
+#include "DX11Manager\DX11Manager.h"
+#include "TextureManager\TextureManager.h"
+
+const D3DXVECTOR2 KingdomParameterUI::m_ParameterPos = D3DXVECTOR2(1100, 100);
+const D3DXVECTOR2 KingdomParameterUI::m_ParameterSize = D3DXVECTOR2(100, 100);
+const D3DXVECTOR2 KingdomParameterUI::m_ParameterTexel[4] =
+{
+	D3DXVECTOR2(0, 0),
+	D3DXVECTOR2(1, 0),
+	D3DXVECTOR2(0, 1),
+	D3DXVECTOR2(1, 1)
+};
+
+
+KingdomParameterUI::KingdomParameterUI()
+{
+	TextureManager::GetInstance()->LoadTexture("File", &m_TextureIndex);
+
+	m_pVertex = new Vertex2D(
+		DX11Manager::GetInstance()->GetDevice(),
+		DX11Manager::GetInstance()->GetDeviceContext(),
+		DX11Manager::GetInstance()->GetWindowHandle());
+
+	m_pVertex->Init(&m_ParameterSize, m_ParameterTexel);
+}
+
+KingdomParameterUI::~KingdomParameterUI()
+{
+	m_pVertex->Release();
+	delete m_pVertex;
+
+	TextureManager::GetInstance()->ReleaseTexture(m_TextureIndex);
+}
+
+void KingdomParameterUI::Control()
+{
+}
+
+void KingdomParameterUI::Draw()
+{
+	m_pVertex->Draw(&m_ParameterPos);
+}
