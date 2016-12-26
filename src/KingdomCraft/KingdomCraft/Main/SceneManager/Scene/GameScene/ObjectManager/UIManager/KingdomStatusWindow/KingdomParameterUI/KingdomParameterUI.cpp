654726dba44206b939ceefc3884c3bab6e288c21
Moto0116 <moto01160902@gmail.com>
2016-12-26 11:28:09 +0900

    [issue #244] KingdomParameterクラスの実装
    - KingdomParameterクラスを実装した

--- src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomStatusWindow/KingdomParameterUI/KingdomParameterUI.cpp
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomStatusWindow/KingdomParameterUI/KingdomParameterUI.cpp
@@ -7,40 +7,44 @@
 #include "DX11Manager\DX11Manager.h"
 #include "TextureManager\TextureManager.h"
 
-const D3DXVECTOR2 KingdomParameterUI::m_ParameterPos = D3DXVECTOR2(110, 10);
-const D3DXVECTOR2 KingdomParameterUI::m_ParameterSize = D3DXVECTOR2(100, 100);
-const D3DXVECTOR2 KingdomParameterUI::m_ParameterTexel[4] =
+const D3DXVECTOR2 KingdomParameterUI::m_ParameterUIPos = D3DXVECTOR2(-230, -65);
+const Vertex::VERTEX KingdomParameterUI::m_ParameterUIVertexData[4] =
 {
-	D3DXVECTOR2(0, 0),
-	D3DXVECTOR2(1, 0),
-	D3DXVECTOR2(0, 1),
-	D3DXVECTOR2(1, 1)
+	Vertex::VERTEX{ D3DXVECTOR3(-15,   0, 0), D3DXVECTOR2(0, 0), D3DXCOLOR(0xffffffff) },	// 政治力
+	Vertex::VERTEX{ D3DXVECTOR3(  0, -15, 0), D3DXVECTOR2(1, 0), D3DXCOLOR(0xffffffff) },	// 戦闘力
+	Vertex::VERTEX{ D3DXVECTOR3(  0,  15, 0), D3DXVECTOR2(0, 1), D3DXCOLOR(0xffffffff) },	// 魔法力
+	Vertex::VERTEX{ D3DXVECTOR3( 15,   0, 0), D3DXVECTOR2(1, 1), D3DXCOLOR(0xffffffff) }	// 工業力
 };
 
 
-KingdomParameterUI::KingdomParameterUI(const D3DXVECTOR2* _pParentPos) :
-m_ParameterTextureIndex(TextureManager::m_InvalidIndex),
-m_pParameterVertex(NULL),
-m_ParentUIPos(*_pParentPos)
+KingdomParameterUI::KingdomParameterUI(const D3DXVECTOR2* _pParentUIPos) :
+m_ParameterUITextureIndex(TextureManager::m_InvalidIndex),
+m_pParameterUIVertex(NULL),
+m_ParentUIPos(*_pParentUIPos),
+m_Parameter(0, 0, 0, 0)
 {
-	TextureManager::GetInstance()->LoadTexture("File", &m_ParameterTextureIndex);
+	TextureManager::GetInstance()->LoadTexture(
+		"Resource\\Texture\\GameScene\\UI\\Parameter.png",
+		&m_ParameterUITextureIndex);
 
-	m_pParameterVertex = new Vertex2D(
+	m_pParameterUIVertex = new Vertex(
 		DX11Manager::GetInstance()->GetDevice(),
 		DX11Manager::GetInstance()->GetDeviceContext(),
 		DX11Manager::GetInstance()->GetWindowHandle());
 
-	m_pParameterVertex->Init(&m_ParameterSize, m_ParameterTexel);
-	m_pParameterVertex->SetTexture(TextureManager::GetInstance()->GetTexture(m_ParameterTextureIndex));
-	m_pParameterVertex->WriteConstantBuffer(&D3DXVECTOR2(m_ParameterPos + m_ParentUIPos));
+	m_pParameterUIVertex->Init();
+	m_pParameterUIVertex->SetTexture(TextureManager::GetInstance()->GetTexture(m_ParameterUITextureIndex));
+	m_pParameterUIVertex->WriteConstantBuffer(&D3DXVECTOR2(m_ParameterUIPos + *_pParentUIPos));
+	m_pParameterUIVertex->CreateVertexBuffer(m_ParameterUIVertexData, 4);
 }
 
 KingdomParameterUI::~KingdomParameterUI()
 {
-	m_pParameterVertex->Release();
-	delete m_pParameterVertex;
+	m_pParameterUIVertex->ReleaseVertexBuffer();
+	m_pParameterUIVertex->Release();
+	delete m_pParameterUIVertex;
 
-	TextureManager::GetInstance()->ReleaseTexture(m_ParameterTextureIndex);
+	TextureManager::GetInstance()->ReleaseTexture(m_ParameterUITextureIndex);
 }
 
 void KingdomParameterUI::Control()
@@ -49,5 +53,21 @@ void KingdomParameterUI::Control()
 
 void KingdomParameterUI::Draw()
 {
-	m_pParameterVertex->Draw();
+	Vertex::VERTEX VertexData[4];
+	for (int i = 0; i < 4; i++)
+	{
+		VertexData[i] = m_ParameterUIVertexData[i];
+	}
+
+	// 王国のパラメーターの大きさでひし形をいじる
+	VertexData[0].Pos.x -= m_Parameter.Politics;
+	VertexData[1].Pos.y -= m_Parameter.CombatPower;
+	VertexData[2].Pos.y += m_Parameter.MagicPower;
+	VertexData[3].Pos.x += m_Parameter.Industry;
+	m_pParameterUIVertex->WriteVertexBuffer(VertexData);
+
+
+	DX11Manager::GetInstance()->SetDepthStencilTest(false);
+	m_pParameterUIVertex->Draw();
+	DX11Manager::GetInstance()->SetDepthStencilTest(true);
 }
--- src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomStatusWindow/KingdomParameterUI/KingdomParameterUI.h
+++ src/KingdomCraft/KingdomCraft/Main/SceneManager/Scene/GameScene/ObjectManager/UIManager/KingdomStatusWindow/KingdomParameterUI/KingdomParameterUI.h
@@ -5,7 +5,7 @@
  */
 #ifndef KINGDOMPARAMETER_H
 #define KINGDOMPARAMETER_H
-#include "Vertex2D\Vertex2D.h"
+#include "Vertex\Vertex.h"
 
 /**
  * 国のパラメータUIを管理するクラス
@@ -14,10 +14,37 @@ class KingdomParameterUI
 {
 public:
 	/**
+	 * 国のパラメータ構造体
+	 */
+	struct KINGDOM_PARAMETER
+	{
+		KINGDOM_PARAMETER() :
+			Politics(0),
+			CombatPower(0),
+			MagicPower(0),
+			Industry(0)
+		{
+		}
+
+		KINGDOM_PARAMETER(int _politics, int _industry, int _combatPower, int _magicPower) :
+			Politics(_politics),
+			CombatPower(_combatPower),
+			MagicPower(_magicPower),
+			Industry(_industry)
+		{
+		}
+
+		int Politics;
+		int CombatPower;
+		int MagicPower;
+		int Industry;
+	};
+
+	/**
 	 * KingdomParameterUIクラスのコンストラクタ
 	 * @param[in] _pParentUIPos 親UIの座標
 	 */
-	KingdomParameterUI(const D3DXVECTOR2* _pParentPos);
+	KingdomParameterUI(const D3DXVECTOR2* _pParentUIPos);
 
 	/**
 	 * KingdomParameterUIクラスのデストラクタ
@@ -35,15 +62,16 @@ public:
 	void Draw();
 
 private:
-	KingdomParameterUI(const KingdomParameterUI&);
+		KingdomParameterUI(const KingdomParameterUI&);
 	void operator=(const KingdomParameterUI&);
 
-	static const D3DXVECTOR2	m_ParameterPos;				//!< パラメータUIを表示する座標
-	static const D3DXVECTOR2	m_ParameterSize;			//!< パラメータUIのサイズ
-	static const D3DXVECTOR2	m_ParameterTexel[4];		//!< パラメータUIのテクスチャ座標
-	Vertex2D*					m_pParameterVertex;			//!< パラメータを表示するVertex2Dクラス
-	int							m_ParameterTextureIndex;	//!< パラメータのテクスチャインデックス
-	D3DXVECTOR2					m_ParentUIPos;				//!< 親UIの座標位置
+	static const D3DXVECTOR2	m_ParameterUIPos;			//!< パラメータUIを表示する座標
+	static const Vertex::VERTEX	m_ParameterUIVertexData[4];	//!< パラメータUIの頂点データの初期値
+
+	Vertex*						m_pParameterUIVertex;
+	int							m_ParameterUITextureIndex;
+	D3DXVECTOR2					m_ParentUIPos;
+	KINGDOM_PARAMETER			m_Parameter;
 
 };
 
